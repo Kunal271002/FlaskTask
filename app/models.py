c65e72038ad610a98f380d8479c2bb1335ff10d6
Kunal271002 <kk.s67533@gmail.com>
2025-06-30 00:19:18 +0530

    Login Completed

--- app/models.py
+++ app/models.py
@@ -2,10 +2,17 @@ from app import db
 
 class Register(db.Model):
     __tablename__ = 'register'
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    UserId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Name = db.Column(db.String(100), nullable=False)
+    Email = db.Column(db.String(100), nullable=False)
+    Password = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return {
+            "User_id": self.UserId,
+            "Name": self.Name,
+            "Email": self.Email
+        }
 
 
 class UserTask(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,57 @@
 from app import create_app,db
 from app.models import Register
 
-from flask import jsonify, render_template,Blueprint,request
+from flask import jsonify, redirect, render_template,Blueprint,request, url_for
 api_bp = Blueprint('api',__name__)
 
 @api_bp.route('/')
-def Home():
-    return render_template("register.html")
+def welcome():
+    return render_template("Welcome.html")
 
-@api_bp.route('/register', methods=["POST", "GET"])
+@api_bp.route('/register')
+def Registeration():
+    return render_template("register.html")
+@api_bp.route('/login')
+def Login():
+    return render_template("Login.html")
+
+@api_bp.route("/ToDoList")
+def ToDoCreate():
+    return "Create TO Do"
+
+
+@api_bp.route('/reg', methods=["POST", "GET"])
 def register_user():
+    
     if request.method == "POST":
         name = request.form['name']
         Email = request.form["email"]
         Password = request.form["password"]
         EnterPassword = request.form["Enterpassword"]
         if Password == EnterPassword:
-            NewUser = Register(name=name, email=Email, password=EnterPassword)
+            NewUser = Register(Name=name, Email=Email, Password=EnterPassword)
             db.session.add(NewUser)
             db.session.commit()
             return jsonify({"message": "Data Added Successfully"})
         else:
             return jsonify({"message": "Password and Re-Password is Different"})
+@api_bp.route('/log', methods=["POST", "GET"])
+def login_user():
+    if request.method == "POST":
+        Email = request.form["email"]
+        Password = request.form["password"]
+        data = Register.query.all()
+        AllEmails = ([item.to_dict()["Email"] for item in data ])
+        Registered = False 
+        for i in AllEmails:
+            if i == Email:
+                Registered = True
+                break
+        if Registered == True:
+            print(Registered)
+            return redirect(url_for("api.ToDoCreate"))
+        else:   
+            return "Please Register"
+
 
         
