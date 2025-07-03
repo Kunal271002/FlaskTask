d3f8c0ef34bbaf50f4b63b1704287e394577dc21
Kunal Sharma <kk.s67533@gmail.com>
2025-07-03 14:03:51 +0530

    Completed

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 import bcrypt
 from app.models import Register,UserTask
+from flask_jwt_extended import create_access_token,jwt_required
 
 from flask import jsonify, redirect, render_template,Blueprint,request, url_for
 api_bp = Blueprint('api',__name__)
@@ -14,18 +15,24 @@ def Registeration():
     return render_template("register.html")
 
 @api_bp.route('/login')
+# @jwt_required
 def Login():
     return render_template("Login.html")
 
 @api_bp.route('/UpdateToDoList/<int:ID>', methods=["POST", "GET"])
+# @jwt_required
+
 def UpdateToDoList(ID):
     return render_template("Update_Page.html",ID=ID)
 
-@api_bp.route('/UpdateToDoList/<int:ID>', methods=["POST", "GET"])
+@api_bp.route('/CreateToDoList/<int:ID>', methods=["POST", "GET"])
+# @jwt_required
+
 def AddTask(ID):
     return render_template("CreateToDoList.html",ID=ID)
 
 @api_bp.route("/ToDoList/<int:ID>")
+# @jwt_required
 def ToDoCreate(ID):
     data = UserTask.query.all()
     All = [item.to_dict() for item in data ]
@@ -50,28 +57,8 @@ def register_user():
         else:
             return jsonify({"message": "Password and Re-Password is Different"})
         
-# @api_bp.route('/log', methods=["POST", "GET"])
-# def login_user():
-#     if request.method == "POST":
-#         Email = request.form["email"]
-#         Password = request.form["password"]
-#         userBytes = Password.encode('utf-8')
-#         data = Register.query.all()
-#         All = [item.to_dict() for item in data]
-#         print(type(All[0]["Password"]))
-#         Correct = bcrypt.checkpw(userBytes, All[0]["Password"])
-#         print(Correct,"Correct or not")
-#         Value = list(filter(lambda x: (x["Email"] == Email and Correct), All))
-#         try:
-#             Id = Value[0]["User_id"]
-#         except:
-#             return "Your Password is wrong or You are not Registered"
-#         Registered =  bool(Value)
-#         if Registered == True:
-#             return redirect(url_for("api.ToDoCreate",ID=Id))
-#         else:   
-#             return "Your Password is wrong or You are not Registered"
 @api_bp.route('/log', methods=["POST", "GET"])
+# @jwt_required
 def login_user():
     if request.method == "POST":
         Email = request.form["email"]
@@ -79,25 +66,21 @@ def login_user():
         userBytes = Password.encode('utf-8')
         data = Register.query.all()
         All = [item.to_dict() for item in data]
-
-        # Filter users by email
         Value = list(filter(lambda x: x["Email"] == Email, All))
-
         if not Value:
             return "You are not registered."
-
-        # Assuming there's only one user with the given email
+        access_token = create_access_token(identity=Email)
         user = Value[0]["Password"]
         stored_hashed_password = user
-        # Check the password
         if bcrypt.checkpw(userBytes, stored_hashed_password.encode('utf-8')):
-            Id = user["User _id"]
+            print(user)
+            Id = Value[0]["User_id"]
             return redirect(url_for("api.ToDoCreate", ID=Id))
         else:
             return "Your password is wrong."
 
-
 @api_bp.route('/AddTask/<int:ID>', methods=["POST", "GET"])
+# @jwt_required
 def CreateToDoList(ID):
     if request.method == "POST":
         Title = request.form["title"]
@@ -115,6 +98,8 @@ def CreateToDoList(ID):
             return jsonify({"message": "Data Added Successfully"})
         
 @api_bp.route('/CompletedToDoList/<int:ID>', methods=["POST", "GET"])
+# @jwt_required
+
 def CompletedToDoList(ID):
     Data = UserTask.query.get_or_404(ID)
     Data.Status = 1
@@ -122,6 +107,8 @@ def CompletedToDoList(ID):
     return "Status Has been Udated"
 
 @api_bp.route('/Update/<int:ID>', methods=["POST", "GET"])
+# @jwt_required
+
 def Update(ID):
     if request.method == "POST":
         Title = request.form["title"]
@@ -133,18 +120,27 @@ def Update(ID):
             Status = 1
         except:
             Status = 0
-        Data = UserTask.query.get_or_404(ID)
-        Data.Status = Status
-        Data.Title = Title
-        Data.Description = discription
-        Data.Due_Date = Date
-        Data.Priority = Priority
-        db.session.commit()
-        print(Title,discription,Date,Priority,Status)
+    else:
+        data = request.get_json()
+        Title = data["title"]
+        discription = data["discription"]
+        Date = data["date"]
+        Priority = data["Priority"]
+        Status = data["Completed"]
+    Data = UserTask.query.get_or_404(ID)
+    Data.Status = Status
+    Data.Title = Title
+    Data.Description = discription
+    Data.Due_Date = Date
+    Data.Priority = Priority
+    db.session.commit()
+    print(Title,discription,Date,Priority,Status)
     return "Status Has been Udated"
 
 @api_bp.route('/DeleteToDoList/<int:ID>', methods=["POST", "GET"])
+# @jwt_required
 def DeleteToDoList(ID):
+
     Data = UserTask.query.get_or_404(ID)
     db.session.delete(Data)
     db.session.commit()
